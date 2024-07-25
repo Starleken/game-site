8f2b71bfd48afe5556948010babd3d89fbb6fbe2
Starleken <108560939+Starleken@users.noreply.github.com>
2024-07-25 11:18:33 +0700

    create docker integration (#32)

--- Backend/history-service/src/test/java/org/leafall/historyservice/HistoryServiceApplicationTests.java
+++ Backend/history-service/src/test/java/org/leafall/historyservice/HistoryServiceApplicationTests.java
@@ -2,8 +2,10 @@ package org.leafall.historyservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(profiles = "local")
 class HistoryServiceApplicationTests {
 
 	@Test
