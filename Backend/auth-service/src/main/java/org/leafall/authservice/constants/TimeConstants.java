ca8dcf7df94823ac0d55ce95ee1cede8c853e01f
Starleken <108560939+Starleken@users.noreply.github.com>
2024-07-27 22:39:33 +0700

    delete tokens by time (#36)

---
+++ Backend/auth-service/src/main/java/org/leafall/authservice/constants/TimeConstants.java
@@ -0,0 +1,7 @@
+package org.leafall.authservice.constants;
+
+public abstract class TimeConstants {
+
+    public final static long DAY = 86400000;
+    public final static long WEEK = 604800000;
+}
